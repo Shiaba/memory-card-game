3001ae1213149cbb0f8f89377e1609765bcc55be
Shiaba <>
2023-08-25 20:39:30 +0000

    Add color change function when clicking on sun and moon button

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Light / Dark button function
+let toggle = document.getElementById('light-dark');
+
+let body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('fa-moon');
+    if(this.classList.toggle('fa-sun')) {
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '1s';
+    } else {
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '1s';
+    }
+})
+
+
