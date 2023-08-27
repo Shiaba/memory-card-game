e9bd65d86e5680ba0a96e2973b3fc620215fae7f
Shiaba <>
2023-08-27 17:19:43 +0000

    Add lockboard so you cant click several other cards during calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 // Memory game
-let cards = document.querySelectorAll('.animal')
+let cards = document.querySelectorAll('.animal');
 let hasFlippedCard = false;
+let lockboard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockboard) return;
     this.classList.add('flip');
 
-    if(!hasFlippedCard) {
+    if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
     } else {
         hasFlippedCard = false;
         secondCard = this;
+
+        //Calculating match
+        if (firstCard.dataset.framework ===
+            secondCard.dataset.framework) {
+            //Disable matched cards    
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            //Unflip if not matching
+        } else {
+            lockboard = true;
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                lockboard = false;
+            }, 750);
+        }
+
     }
 }
 
@@ -22,9 +41,9 @@ let toggle = document.getElementById('light-dark');
 
 let body = document.querySelector('body');
 
-toggle.addEventListener('click', function(){
+toggle.addEventListener('click', function () {
     this.classList.toggle('fa-moon');
-    if(this.classList.toggle('fa-sun')) {
+    if (this.classList.toggle('fa-sun')) {
         body.style.background = 'white';
         body.style.color = 'black';
         body.style.transition = '1s';
@@ -33,6 +52,6 @@ toggle.addEventListener('click', function(){
         body.style.color = 'white';
         body.style.transition = '1s';
     }
-})
+});
 
 
